a6538710ec6a985bf00e20159618a92579121ab2
JustinvdLans <42778559+JustinvdLans@users.noreply.github.com>
2019-04-05 15:10:02 +0200

    Add files via upload

---
+++ Assets/Scripts/Items/coins/CoinScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+	public GameObject player; 
+	public Player playerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        playerScript = player.GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Funtion for a coin
+    void OnTriggerEnter2D(Collider2D other)
+    {
+    	if(other.CompareTag("Player"))
+    	{
+    		Debug.Log(playerScript.points);
+    		playerScript.points++;
+    		//Add 1 to points.
+    		Destroy(gameObject); // this destroys things
+    	
+    	}
+    }
+}
---
+++ Assets/Scripts/Players/Player/Player.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	//ExitTime UIT anders gaat t fout
+
+	// Components koppelen aan script
+	public Animator anim;
+	public Rigidbody2D rb;
+
+	public float speed;
+	public float maxSpeed;
+	public float jumpForce;
+    public int points = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+  
+
+    void FixedUpdate()
+    {
+
+
+    	// Walk buttons
+		float h =  Input.GetAxis("Horizontal");
+
+        rb.AddForce((Vector2.right * speed) * h);
+
+        if(rb.velocity.x > maxSpeed)
+        {
+        	rb.velocity = new Vector2(maxSpeed, rb.velocity.y);
+        }
+
+        if(rb.velocity.x < -maxSpeed)
+        {
+        	rb.velocity = new Vector2(-maxSpeed, rb.velocity.y);
+        }
+    }
+
+
+     void Update()
+    {
+    	//jump function
+    	if(Input.GetButtonDown("Jump"))
+    	{
+    		rb.AddForce(Vector2.up * jumpForce);
+    	}
+
+    	// Walk animations
+        anim.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+
+        if(Input.GetAxis("Horizontal") < -0.1f)
+        {
+        	transform.localScale = new Vector3(-0.7f,0.7f,1);
+        }
+        if(Input.GetAxis("Horizontal") > 0.1f)
+        {
+        	transform.localScale = new Vector3(0.7f,0.7f,1);
+        }
+
+    } 
+        private void OnGUI()
+    {
+        GUI.Label(new Rect(10,10,100,20), "Score : " + points);
+    }
+    
+}
